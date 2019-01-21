e7ee8c7affd7b9f2727ea9b26b631ef0208b8580
wuming <liumingyang@zhuaqianmao.com>
2019-01-21 18:57:43 +0800

    hashmap8

--- src/main/java/com/cx/collections/hashmap/MyHashMap.java
+++ src/main/java/com/cx/collections/hashmap/MyHashMap.java
@@ -2,6 +2,7 @@ package com.cx.collections.hashmap;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 
 public class MyHashMap {
@@ -16,6 +17,14 @@ public class MyHashMap {
     }
 
     public static void main(String[] args) {
-        System.out.println(1 << 4);
+        final HashMap<String, String> map = new HashMap<String, String>();
+        for (int i = 0; i < 1000; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    map.put(UUID.randomUUID().toString(), "");
+                }
+            }).start();
+        }
     }
 }
